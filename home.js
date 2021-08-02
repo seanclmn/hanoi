280b32287e595682deae8cf0d2d8847af7ad4150
secolema <secolema@iu.edu>
2021-08-02 14:51:23 -0400

    added background and added a home page

---
+++ home.js
@@ -0,0 +1,22 @@
+const rules = document.querySelector(".rules")
+const modal = document.querySelector("#modal")
+const close_modal = document.querySelector("#close")
+
+
+let top_score = null
+
+//Function for closing modal
+function close_the_modal(){
+    modal.style.display="none"
+    draggables.forEach(item =>{
+        containers[0].appendChild(item)
+    })
+    counter.innerHTML=0
+}
+// close_the_modal()
+close_modal.addEventListener("click",close_the_modal)
+
+rules.addEventListener("click",function(){
+    modal.style.display="block"
+})
+
--- script.js
+++ script.js
@@ -2,27 +2,28 @@ let draggables = document.querySelectorAll(".draggable")
 let containers = document.querySelectorAll(".container")
 let restart = document.querySelector("#restart")
 let counter = document.querySelector("#counter")
+const modal = document.querySelector("#modal")
+const close_modal = document.querySelector("#close")
 
 
+let top_score = null
 
-
-let modal = document.getElementById("modal")
-let close_modal = document.getElementById("close")
-
-let topscore = null
-
-let parent_id = null
-
-
+//Function for closing modal
+function close_the_modal(){
+    modal.style.display="none"
+    draggables.forEach(item =>{
+        containers[0].appendChild(item)
+    })
+    counter.innerHTML=0
+}
+// close_the_modal()
+close_modal.addEventListener("click",close_the_modal)
 
 
 update_draggable()
 
-close_modal.addEventListener("click",function(){
-    modal.style.display="none"
 
-})
-
+//This restarts the game. Counter is set to 0 and the game is restarted.
 restart.addEventListener("click",function(){
     draggables.forEach(item =>{
         containers[0].appendChild(item)
@@ -33,7 +34,6 @@ restart.addEventListener("click",function(){
 draggables.forEach(item => {
     item.addEventListener("dragstart",()=>{
         parent_id = item.parentElement.id
-        console.log(parent)
         item.classList.add("dragging_now")
     })
     item.addEventListener("dragend",()=>{
@@ -49,29 +49,26 @@ containers.forEach(container =>{
     container.addEventListener("dragover",(event)=>{
         event.preventDefault()
         let draggable = document.querySelector(".dragging_now")
-
         let draggable_array = container.querySelectorAll(".draggable")
         let target = draggable_array[draggable_array.length-1]
         if(draggable_array.length==0 || draggable.id[1]>target.id[1]){
             container.appendChild(draggable)
             check_win()
-
         }
     })
-
 })
 
 
+
+//This function checks for a win. If the game is won, a victory modal is displayed.
 function check_win(){
     if(containers[0].querySelector(".draggable") == null && containers[1].querySelector(".draggable") == null){
         modal.style.display="block"
-
     }
 }
 
+//This function makes the top div of each container draggable
 function update_draggable(){
-    //This function makes the top div of each container draggable
-    console.log(`<br>`)
     for(i=0;i<3;i++){
         let x= document.querySelector(`#container_${i+1}`).querySelectorAll(".draggable")
         let top = x[x.length - 1]
@@ -79,18 +76,14 @@ function update_draggable(){
             x.forEach(item =>{
                 if(item!==undefined){
                     if(item==top){
-                        console.log(item)
                         item.setAttribute('draggable', true)
                     }else{
                         item.setAttribute('draggable', false)
                     }
-        
                 }
-            
             }
-
         )
-
-
     }
 }
+
+
